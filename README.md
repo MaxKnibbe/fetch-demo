2b75211421148306159ad9ab4e9c49bcfae38ecc
Max Knibbe <maxwellknibbe@gmail.com>
2021-11-09 21:14:54 -0800

    Got data to work

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # fetch-demo
+
+## GIT Push
+- `git add .`
+- `git commit -m "something here"`
+- `git push`
+
--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,1 +1,29 @@
-console.log("does this work");
+const url = "https://swapi.dev/api/people/4?format=json";
+
+(async () => {
+    try {
+        //Get Data
+        const response = await fetch(url);
+        const data = await response.json();
+
+        //Select DOM elements
+        const el = { 
+            name : document.getElementById("name"),
+            eye_color : document.getElementById("eye_color")
+        };
+
+        console.log({el});
+
+        //Hydrate DOM elements with data
+        const hydrate = () => {
+            el.name.innerHTML = data.name;
+            el.eye_color.innerHTML = data.eye_color;
+            el.eye_color.style.color = data.eye_color;
+        }
+
+        hydrate();
+    } catch(error) {
+        console.log(error);
+    }
+})()
+
